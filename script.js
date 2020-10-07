b5380c697562cc99667c7d486b6d7cc38a465d8b
Luca Safons <lucasafons@outlook.com>
2020-10-06 21:37:07 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var messages = document.getElementById("conversa");
+var text1 = document.getElementById("text1");
+var text2 = document.getElementById("text2");
+var botao1 = document.getElementById("submit1");
+var botao2 = document.getElementById("submit2");
+
+botao1.addEventListener("click", function () {
+  var newMessage = document.createElement("ul");
+  newMessage.innerHTML = text1.value;
+  messages.appendChild(newMessage);
+  text1.value = "";
+});
+
+botao2.addEventListener("click", function () {
+  var newMessage = document.createElement("lu");
+  newMessage.innerHTML = text2.value;
+  messages.appendChild(newMessage);
+  text2.value = "";
+});
